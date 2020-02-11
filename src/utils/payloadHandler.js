ca128cbfb820fa8cd38df84f1698c7a5dbbcb3b7
wizo06 <14183421+wizo06@users.noreply.github.com>
2020-02-11 04:59:28 -0500

    Added payloadHandler.js

---
+++ src/utils/payloadHandler.js
@@ -0,0 +1,27 @@
+module.exports = payloadHandler = {
+  /**
+   * Checks if payload has all required keys
+   * @param {{Object}} payload
+   * @param {{Array}} requiredKeys
+   * @param {{boolean}}
+   */
+  payloadHasAllRequiredKeys: (payload, requiredKeys) => {
+    return requiredKeys.every(key => typeof payload[key] !== 'undefined');
+  },
+
+  /**
+   * Checks if payload has a valid schema.
+   * Currently this only works for one level deep keys.
+   * @param {{Object}} payload
+   * @param {{Object}} schema
+   * @return {{boolean}}
+   */
+  payloadHasValidSchema: (payload, schema) => {
+    const payloadKeys = Object.keys(payload);
+    return payloadKeys.every(key => {
+      if (schema[key] === 'array') return Array.isArray(payload[key]);
+      if (schema[key] === 'object') return typeof payload[key] === 'object' && !!payload[key];
+      return schema[key] === typeof payload[key];
+    });
+  },
+};
