d6a4c5feb135d572c9906c8ef992cfb728d524c6
wizo06 <14183421+wizo06@users.noreply.github.com>
2020-02-11 08:50:49 -0500

    updated pipeline.js

--- src/pipeline.js
+++ src/pipeline.js
@@ -6,23 +6,29 @@ const logger = require(path.join(process.cwd(), 'src/utils/logger.js'));
 const queueHandler = require(path.join(process.cwd(), 'src/utils/queueHandler.js'));
 const rclone = require(path.join(process.cwd(), 'src/automata/rclone.js'));
 
-module.exports = processNextJob = () => {
-  // Step 0 Retrieve next job
-  const job = queueHandler.getFirstJob();
+module.exports = processNextJob = async () => {
+  try {
+    // Step 0 Retrieve next job
+    const job = queueHandler.getFirstJob();
 
-  // Step 1 Download
-  logger.info('[1/4] Downloading');
-  rclone.download(job);
+    // Step 1 Download
+    logger.info('[1/4] Downloading');
+    await rclone.download(job);
 
-  // Step 2 Hardsub
-  transcode.x264(job);
+    // Step 2 Hardsub
+    await transcode.x264(job);
 
-  // Step 3 Upload
-  rclone.upload(job);
+    // Step 3 Upload
+    await rclone.upload(job);
 
-  // Step 4 Notify
-  notification.send(job);
+    // Step 4 Notify
+    await notification.send(job);
 
-  // Check if queue has jobs and recursively process next job
-  if (!queueHandler.isEmpty()) processNextJob();
+    // Check if queue has jobs and recursively process next job
+    if (!queueHandler.isEmpty()) processNextJob();
+  }
+  catch (e) {
+    if (e === 3) logger.error('Download failed');
+
+  }
 };
