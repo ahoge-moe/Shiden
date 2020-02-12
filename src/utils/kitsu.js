09509f97d6f5609f5f4d17f5cdfb34323fc1c68e
wizo06 <14183421+wizo06@users.noreply.github.com>
2020-02-11 19:36:15 -0500

    added kitsu.js

---
+++ src/utils/kitsu.js
@@ -0,0 +1,45 @@
+/**
+ * @module kitsu
+ * This module handles requests to Kitsu
+ */
+
+// Import node modules
+const path = require('path');
+
+// Import custom modules
+const promisefied = require(path.join(process.cwd(), 'src/utils/promisefied.js'));
+
+module.exports = Kitsu = {
+  query: showName => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const options = {
+          url: encodeURI(`https://kitsu.io/api/edge/anime?filter[text]=${showName}`),
+          method: 'GET',
+        };
+        const response = await promisefied.request(options);
+
+        if (response.res.statusCode === 200) {
+          const parsedBody = JSON.parse(response.body);
+          if (parsedBody.data[0]) {
+            if (parsedBody.data[0].attributes) {
+              return resolve(parsedBody.data[0].attributes.slug);
+            }
+            else {
+              return resolve('');
+            }
+          }
+          else {
+            return resolve('');
+          }
+        }
+        else {
+          return resolve('');
+        }
+      }
+      catch (e) {
+        return resolve('');
+      }
+    });
+  },
+};
